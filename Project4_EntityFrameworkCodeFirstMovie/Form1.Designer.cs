bb81888544e29fb8e33f07421cc6b5057562120e
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2024-11-21 23:32:51 +0300

    4 ve 5. Proje

---
+++ Project4_EntityFrameworkCodeFirstMovie/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Project4_EntityFrameworkCodeFirstMovie
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnList = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtId = new System.Windows.Forms.TextBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCreate = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnSearch = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnList
+			// 
+			this.btnList.Location = new System.Drawing.Point(241, 19);
+			this.btnList.Name = "btnList";
+			this.btnList.Size = new System.Drawing.Size(103, 26);
+			this.btnList.TabIndex = 0;
+			this.btnList.Text = "Listele";
+			this.btnList.UseVisualStyleBackColor = true;
+			this.btnList.Click += new System.EventHandler(this.btnList_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(19, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(64, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Kategori Id :";
+			// 
+			// txtId
+			// 
+			this.txtId.Location = new System.Drawing.Point(86, 23);
+			this.txtId.Name = "txtId";
+			this.txtId.Size = new System.Drawing.Size(145, 20);
+			this.txtId.TabIndex = 2;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(22, 144);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(322, 230);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(86, 49);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(145, 20);
+			this.txtName.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 52);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(67, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Kategori Adı:";
+			// 
+			// btnCreate
+			// 
+			this.btnCreate.Location = new System.Drawing.Point(241, 51);
+			this.btnCreate.Name = "btnCreate";
+			this.btnCreate.Size = new System.Drawing.Size(103, 26);
+			this.btnCreate.TabIndex = 6;
+			this.btnCreate.Text = "Ekle";
+			this.btnCreate.UseVisualStyleBackColor = true;
+			this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Location = new System.Drawing.Point(241, 83);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(103, 26);
+			this.btnDelete.TabIndex = 7;
+			this.btnDelete.Text = "Sil";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.Location = new System.Drawing.Point(19, 83);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(103, 26);
+			this.btnUpdate.TabIndex = 8;
+			this.btnUpdate.Text = "Güncelle";
+			this.btnUpdate.UseVisualStyleBackColor = true;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+			// 
+			// btnSearch
+			// 
+			this.btnSearch.Location = new System.Drawing.Point(128, 83);
+			this.btnSearch.Name = "btnSearch";
+			this.btnSearch.Size = new System.Drawing.Size(103, 26);
+			this.btnSearch.TabIndex = 9;
+			this.btnSearch.Text = "Ara";
+			this.btnSearch.UseVisualStyleBackColor = true;
+			this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(368, 396);
+			this.Controls.Add(this.btnSearch);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnCreate);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.txtId);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnList);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnList;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtId;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnCreate;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.Button btnSearch;
+	}
+}
+
---
+++ Project4_EntityFrameworkCodeFirstMovie/Migrations/202411211944420_mig3.cs
@@ -0,0 +1,20 @@
+﻿namespace Project4_EntityFrameworkCodeFirstMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "Duration", c => c.Int(nullable: false));
+            DropColumn("dbo.Movies", "Deration");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Movies", "Deration", c => c.Int(nullable: false));
+            DropColumn("dbo.Movies", "Duration");
+        }
+    }
+}
