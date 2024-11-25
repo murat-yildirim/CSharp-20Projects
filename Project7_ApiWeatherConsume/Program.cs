532d58666a7748812804b0eb532d4f26b82dec1c
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2024-11-26 00:03:36 +0300

    7.Proje Sonu

---
+++ Project7_ApiWeatherConsume/Program.cs
@@ -0,0 +1,167 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Xml;
+#region Menü Başlangıcı
+Console.WriteLine("Api Consume İşlemine Hoş Geldiniz");
+Console.WriteLine();
+Console.WriteLine("### Yapmak İstediğiniz İşlemi Seçin ###");
+Console.WriteLine();
+Console.WriteLine("1-Şehir Listesine Getirin");
+Console.WriteLine("2-Şehir ve Hava Durumu Listesini Getirin");
+Console.WriteLine("3-Yeni Şehir Ekleme");
+Console.WriteLine("4-Şehir Silme İşlemi");
+Console.WriteLine("5-Şehir Güncelleme İşlemi");
+Console.WriteLine("6-ID'ye Göre ŞEhir Getirme");
+Console.WriteLine();
+#endregion
+string number;
+
+Console.Write("Tercihiniz: ");
+number = Console.ReadLine();
+Console.WriteLine();
+if (number == "1")
+{
+	string url = "https://localhost:7239/api/Weathers";
+	using (HttpClient client = new HttpClient()) 
+	{
+		HttpResponseMessage response = await client.GetAsync(url);
+		string responseBody = await response.Content.ReadAsStringAsync();
+		JArray jArray = JArray.Parse(responseBody);
+		foreach (var item in jArray)
+		{
+			string cityName = item["cityName"].ToString();
+			Console.WriteLine($"Şehir: {cityName}");
+		}
+	}
+}
+if (number == "2")
+{
+	string url = "https://localhost:7239/api/Weathers";
+	using (HttpClient client = new HttpClient())
+	{
+		HttpResponseMessage response = await client.GetAsync(url);
+		string responseBody = await response.Content.ReadAsStringAsync();
+		JArray jArray = JArray.Parse(responseBody);
+		foreach (var item in jArray)
+		{
+			string cityName = item["cityName"].ToString();
+			string temp = item["temp"].ToString();
+			string country = item["country"].ToString();
+			Console.WriteLine(cityName + "-" + country + "-->" + temp + " Derece");
+			Console.WriteLine("-------------------------------------------------------");
+		}
+	}
+}
+if (number == "3")
+{
+	Console.WriteLine("### Yeni Veri Girişi ###");
+	Console.WriteLine();
+	string cityName, country, detail;
+	decimal temp;
+	Console.Write("Şehir Adı: ");
+	cityName = Console.ReadLine();
+	Console.Write("Ülke Adı: ");
+	country = Console.ReadLine();
+	Console.Write("Hava Durumu Detayı");
+	detail = Console.ReadLine();
+	Console.Write("Sıcaklık: ");
+	temp = decimal.Parse(Console.ReadLine());
+	string url = "https://localhost:7239/api/Weathers";
+	var newWeatherCity = new
+	{
+		CityName = cityName,
+		Country = country,
+		Detail = detail,
+		Temp = temp
+
+	};
+	using (HttpClient client = new HttpClient())
+	{
+		string json = JsonConvert.SerializeObject(newWeatherCity);
+		StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+		HttpResponseMessage response = await client.PostAsync(url, content); 
+		response.EnsureSuccessStatusCode(); 
+	}
+}
+if (number == "4")
+{
+	string url = "https://localhost:7239/api/Weathers?id=";
+
+	Console.Write("Silmek İstediğiniz Id Değeri: ");
+	int id = int.Parse(Console.ReadLine());
+	using (HttpClient client = new HttpClient())
+	{
+		HttpResponseMessage response = await client.DeleteAsync(url + id);
+		response.EnsureSuccessStatusCode();
+	}
+}
+if (number == "5")
+{
+	string url = "https://localhost:7239/api/Weathers";
+	Console.WriteLine("### Veri Güncelleme İşlemi ###");
+	Console.WriteLine();
+	string cityName, country, detail;
+	decimal temp;
+	int cityId;
+
+	Console.Write("Şehir Adı: ");
+	cityName = Console.ReadLine();
+
+	Console.Write("Ülke Adı: ");
+	country = Console.ReadLine();
+
+	Console.Write("Hava Durumu Detayı: ");
+	detail = Console.ReadLine();
+
+	Console.Write("Sıcaklık: ");
+	temp = decimal.Parse(Console.ReadLine());
+
+	Console.Write("Şehir Id: ");
+	cityId = int.Parse(Console.ReadLine());
+
+	var updatedWeatherValues = new
+	{
+		CityId = cityId,
+		CityName = cityName,
+		Country = country,
+		Detail = detail,
+		Temp = temp
+	};
+
+	using (HttpClient client = new HttpClient())
+	{
+		string json = JsonConvert.SerializeObject(updatedWeatherValues);
+		StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+		HttpResponseMessage response = await client.PutAsync(url, content);
+		response.EnsureSuccessStatusCode();
+	}
+}
+if (number == "6")
+{
+	string url = "https://localhost:7239/api/Weathers/GetByIdWeatherCity?id=";
+
+	Console.Write("Bilgilerini Getirmek İstediğiniz Id Değeri: ");
+	int id = int.Parse(Console.ReadLine());
+	Console.WriteLine();
+
+	using (HttpClient client = new HttpClient())
+	{
+		HttpResponseMessage response = await client.GetAsync(url + id);
+		response.EnsureSuccessStatusCode();
+		string responseBody = await response.Content.ReadAsStringAsync();
+		JObject weatherCityOBject = JObject.Parse(responseBody);
+
+		string cityName = weatherCityOBject["cityName"].ToString();
+		string detail = weatherCityOBject["detail"].ToString();
+		string country = weatherCityOBject["country"].ToString();
+		decimal temp = decimal.Parse(weatherCityOBject["temp"].ToString());
+
+		Console.Write("Girmiş olduğunuz Id değerleri ait bilgiler");
+		Console.WriteLine();
+		Console.Write("Şehir: " + cityName + " Ülke: " + country + " Detay: " + detail + " Sıcaklık: " + temp);
+	}
+}
+
+Console.Read();
